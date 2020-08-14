3af0bb1df62a78ff828b1a3767ab83161612d59b
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 14:30:44 +0530

    Types of pramotion added single product big quantity OR group of products

---
+++ PramotionEngine/PramotionEngineConsole/IMultipleProductClubbPramotion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PramotionEngineConsole
+{
+    interface IMultipleProductClubbPramotion
+    {
+        //Method to return discounted price of multiple products (SKU IDs)
+        double ClubbPramotion(int product_C, int product_D);
+
+        //Same can be overload for more than tow products
+        //double ClubbPramotion(int product_A, int product_A_Quantity, int product_B, int product_B_Quantity);
+    }
+}
---
+++ PramotionEngine/PramotionEngineConsole/ISingleProductQuantityPramotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PramotionEngineConsole
+{
+    interface ISingleProductQuantityPramotion
+    {
+        //Method to return discounted price of single product (SKU ID)
+        double QuantityPramotion(int quantity);
+    }
+}
