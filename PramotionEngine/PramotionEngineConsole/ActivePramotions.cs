456a836a9c52ff6ad571de125344528b970eb46e
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 14:40:26 +0530

    active pramotions added using type of pramotions interface which can be used further for future pramotions

---
+++ PramotionEngine/PramotionEngineConsole/ActivePramotions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PramotionEngineConsole
+{
+    class ActivePramotions : Products
+    {
+        public int ThreeAGroup { get; set; } = 3;
+        public int TwoBGroup { get; set; } = 2;
+        public int CPlusDGroup { get; set; } = 2;
+    }
+
+
+    //Pramotion A
+    public class ThreeAPramotion : ISingleProductQuantityPramotion
+    {
+        public double QuantityPramotion(int quantity)
+        {
+            return 0;
+        }
+    }
+
+    //Pramotion B
+    public class TwoBPramotion :  ISingleProductQuantityPramotion
+    {
+        public double QuantityPramotion(int quantity)
+        {
+            return 0;
+        }
+    }
+
+    //Pramotion C + D
+    public class CPlusDPramotion : IMultipleProductClubbPramotion
+    {
+        public double ClubbPramotion(int product_C, int product_D)
+        {
+            return 0;
+        }
+    }
+
+
+
+
+
+}
