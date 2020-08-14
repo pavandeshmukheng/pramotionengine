35e579563ad4c21d2291967f3d9c222bd7c353f5
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 13:46:25 +0530

    Initial check in PramotionEngineConsole

---
+++ PramotionEngine/PramotionEngineConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PramotionEngineConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
