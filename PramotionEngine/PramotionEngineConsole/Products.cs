d11ff05b4750c3c94c5407022497d3c93e286d6f
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 14:11:36 +0530

    list of SKU IDs with fixed price added (consider list of products with their prices)

---
+++ PramotionEngine/PramotionEngineConsole/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PramotionEngineConsole
+{
+    //Products with fixed price SKU IDs
+    public class Products
+    {
+        public Dictionary<string, int> SKU_ID_List { get; set; }
+        public Products()
+        {
+            SKU_ID_List = new Dictionary<string, int>
+            {
+                { "A", 50 },
+                { "B", 30 },
+                { "C", 20 },
+                { "D", 15 }
+            };
+        }
+    }
+}
