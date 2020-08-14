55afc169a68ca15a57ebf21b04935bba699c530a
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 15:09:31 +0530

    test case ready implementation provided for actual pramotions

--- PramotionEngine/PramotionEngineConsole/ShoppingClass.cs
+++ PramotionEngine/PramotionEngineConsole/ShoppingClass.cs
@@ -9,8 +9,8 @@ namespace PramotionEngineConsole
         public double ShoppingCart(int A_Quantity = 0, int B_Quantity = 0, int C_Quantity = 0, int D_Quantity = 0)
         {
             double ATotal = new ThreeAPramotion().QuantityPramotion(A_Quantity);
-            double BTotal = 0;
-            double CDTotal = 0;
+            double BTotal = new TwoBPramotion().QuantityPramotion(B_Quantity);
+            double CDTotal = new CPlusDPramotion().ClubbPramotion(C_Quantity, D_Quantity);
             return Math.Round(ATotal + BTotal + CDTotal);            
         }
     }
