6d528876ebd19cff83994e2c0d3c939e433ff91a
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 13:50:30 +0530

    PramotionEngine Test Case Project Added

---
+++ PramotionEngine/PramotionEngineUnitTest/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PramotionEngineUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
