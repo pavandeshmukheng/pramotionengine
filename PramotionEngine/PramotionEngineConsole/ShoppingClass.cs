3e25bc47326c4d09e2369a21eee3813a766bf348
PAVAN DESHMUKH <pavan.deshmukh@volkswagen.co.in>
2020-08-14 14:01:46 +0530

    Expected test cases (Scenario A B, C) and (when one pramotion applied other pramotions will not be applied) is added

---
+++ PramotionEngine/PramotionEngineConsole/ShoppingClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PramotionEngineConsole
+{
+    public class ShoppingClass
+    {
+        public double ShoppingCart(int A_Quantity = 0, int B_Quantity = 0, int C_Quantity = 0, int D_Quantity = 0)
+        {
+            return 1;
+        }
+    }
+}
--- PramotionEngine/PramotionEngineUnitTest/UnitTest.cs
+++ PramotionEngine/PramotionEngineUnitTest/UnitTest.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PramotionEngineConsole;
+
 
 namespace PramotionEngineUnitTest
 {
@@ -6,8 +8,41 @@ namespace PramotionEngineUnitTest
     public class UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ScenarioA()
         {
+            //Mention quantity of SKU ID's(Products) in order A,B,C,D
+            ShoppingClass obj = new ShoppingClass();
+            double result = obj.ShoppingCart(1, 1, 1, 0);
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void ScenarioB()
+        {
+            //Mention quantity of SKU ID's(Products) in order A,B,C,D
+            ShoppingClass obj = new ShoppingClass();
+            double result = obj.ShoppingCart(5, 5, 1, 0);
+            Assert.AreEqual(370, result);
+        }
+
+        [TestMethod]
+        public void ScenarioC()
+        {
+            //Mention quantity of SKU ID's(Products) in order A,B,C,D
+            ShoppingClass obj = new ShoppingClass();
+            double result = obj.ShoppingCart(3, 5, 1, 1);
+            Assert.AreEqual(280, result);
+        }
+
+        //If one pramotion applied other pramotions will not be applied
+        //The pramotion which is system recommendation is best value saving for customer
+        [TestMethod]
+        public void ScenarioToGetBestMatchOfferOnTheBasisOfSelectedProductAndQuantity()
+        {
+            //Mention quantity of SKU ID's(Products) in order A,B,C,D
+            ShoppingClass obj = new ShoppingClass();
+            double result = obj.ShoppingCart(30, 35, 10, 10);
+            Assert.AreEqual(1000, result);
         }
     }
 }
